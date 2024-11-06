342b9cca95cd86ac3f08b31f8f6784f8d57d0786
ahmedsnippet <ahmedsnippet@gmail.com>
2024-11-05 23:21:20 -0800

    Url_Setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ config({
 })
 
 app.use(cors({
-    origin: "https://hospital-mern-onk5.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }))
